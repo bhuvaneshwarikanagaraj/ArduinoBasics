179014ccc5263683c132bb0f71c75bb9d16831c9
bhuvaneshwarikanagaraj <bhuvaneshwarii026@gmail.com>
2023-04-09 17:54:12 +0530

    Loop()

---
+++ Chapter15-UnderstandingLoops/ForLopp.ino
@@ -0,0 +1,21 @@
+int YellowPin =6;
+int redPin=9;
+int delayTime=1000;
+void setup(){
+ pinMode(YellowPin,OUTPUT);
+ pinMode(redPin,OUTPUT);
+
+}
+void loop(){
+    for(int i=0; i<=5 ; i++){
+        digitalWrite(YellowPin,HIGH);
+        delay(delayTime);
+        digitalWrite(YellowPin,LOW);
+    }
+
+    for(int j=0 ; j<=2 ; j++){
+        digitalWrite(redPin,HIGH);
+        delay(delayTime);
+        digitalWrite(redPin,LOW);
+    }
+}
